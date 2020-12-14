3d2772477ad5abb2b6d0b342e3362473255c5e1a
LaShawn <lts001@gmail.com>
2020-12-14 21:59:13 +0900

    testing heroku

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path')
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
 
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
 
 const port = process.env.PORT || 8080;
-app.listen(port)
+app.listen(port);
 
 console.log(`Listening on port ${port}`);
-
